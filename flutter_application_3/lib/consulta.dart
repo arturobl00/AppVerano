c4faf34de191bf41a8442286f8ca9422f6a12b5f
arturobl00 <arturobl00@msn.com>
2022-07-20 15:46:34 -0500

    Actualizacion 20

--- flutter_application_3/lib/consulta.dart
+++ flutter_application_3/lib/consulta.dart
@@ -10,6 +10,64 @@ class Consulta extends StatefulWidget {
 }
 
 class _ConsultaState extends State<Consulta> {
+  TextEditingController controllerNombre = TextEditingController();
+  TextEditingController controllerApellidos = TextEditingController();
+
+  Future<void> _update([DocumentSnapshot? documentSnapshot]) async {
+    if (documentSnapshot != null) {
+      controllerNombre.text = documentSnapshot['nombre'];
+      controllerApellidos.text = documentSnapshot['apellidos'];
+    }
+
+    await showModalBottomSheet(
+        isScrollControlled: true,
+        context: context,
+        builder: (BuildContext ctx) {
+          return Padding(
+            padding: EdgeInsets.only(
+                top: 20,
+                left: 20,
+                right: 20,
+                bottom: MediaQuery.of(ctx).viewInsets.bottom + 20),
+            child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  TextField(
+                    controller: controllerNombre,
+                    decoration: const InputDecoration(labelText: 'Nombre'),
+                  ),
+                  TextField(
+                    controller: controllerApellidos,
+                    decoration: const InputDecoration(labelText: 'Apellidos'),
+                  ),
+                  const SizedBox(
+                    height: 20.0,
+                  ),
+                  ElevatedButton(
+                      onPressed: () async {
+                        final String nombre = controllerNombre.text;
+                        final String apellidos = controllerApellidos.text;
+                        await FirebaseFirestore.instance
+                            .collection("verano")
+                            .doc(documentSnapshot!.id)
+                            .update({"nombre": nombre, "apellidos": apellidos});
+
+                        Navigator.pop(context);
+                      },
+                      child: const Text("Actualizar"))
+                ]),
+          );
+        });
+  }
+
+  Future<void> _delete([DocumentSnapshot? documentSnapshot]) async {
+    await FirebaseFirestore.instance
+        .collection('verano')
+        .doc(documentSnapshot!.id)
+        .delete();
+  }
+
   @override
   Widget build(BuildContext context) {
     return SizedBox(
@@ -26,6 +84,8 @@ class _ConsultaState extends State<Consulta> {
                       physics: const ScrollPhysics(),
                       itemCount: snapshot.data!.docs.length,
                       itemBuilder: (context, i) {
+                        final DocumentSnapshot documentSnapshot =
+                            snapshot.data!.docs[i];
                         QueryDocumentSnapshot x = snapshot.data!.docs[i];
                         return Row(
                             mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -43,13 +103,17 @@ class _ConsultaState extends State<Consulta> {
                                     padding: const EdgeInsets.only(
                                         right: 8.0, top: 8.0),
                                     child: FloatingActionButton(
-                                      onPressed: () {},
+                                      onPressed: () {
+                                        _update(documentSnapshot);
+                                      },
                                       backgroundColor: Colors.green,
                                       child: const Icon(Icons.edit),
                                     ),
                                   ),
                                   FloatingActionButton(
-                                    onPressed: () {},
+                                    onPressed: () {
+                                      _delete(documentSnapshot);
+                                    },
                                     backgroundColor: Colors.red,
                                     child: const Icon(Icons.delete),
                                   )
